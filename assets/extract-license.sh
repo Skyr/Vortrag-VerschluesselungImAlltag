46b7489bff1889e1bf7412dfc7a9e7b3b026bb43
Stefan Schlott <stefan@ploing.de>
2012-02-19 12:02:54 +0100

    Bildnachweise

---
+++ assets/extract-license.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+EMPTY=""
+for i in *.jpg ; do
+	COMMENT=`exiv2 -q -Pt -g Exif.Photo.UserComment $i`
+	if [ -n "$COMMENT" ] ; then
+		LINK=`echo $COMMENT | sed "s/^.*Source: \([^ ]*\).*$/\1/"`
+		LICENSE=`echo $COMMENT | sed "s/^.*License: \([^ ]*\).*$/\1/"`
+		LICENSE=`echo $LICENSE | sed "s/Attribution/BY/;s/NonCommercial/NC/;s/ShareAlike/SA/;s/NoDerivs/ND/"`
+		echo "<li><a href=\"$LINK\">$LINK</a> CC $LICENSE</li>"
+	else
+		EMPTY="$i $EMPTY"
+	fi
+done
+if [ -n "$EMPTY" ] ; then
+	echo "Please check manually: $EMPTY" >&2
+fi
+
